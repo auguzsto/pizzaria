b84fe62a62b9deb525d2e95aaf4a90f0407d6eea
Augusto <auguzsto@gmail.com>
2023-06-14 15:10:35 -0300

    fix web page

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -3,8 +3,7 @@ import 'package:get/get.dart';
 import 'package:pizzaria/src/shared/models/user_model.dart';
 
 import 'package:pizzaria/src/shared/themes/themes.dart';
-import 'package:pizzaria/src/views/client/home/home_screen.dart';
-//import 'package:pizzaria/src/views/main/main_view.dart';
+import 'package:pizzaria/src/views/main/main_view.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
@@ -15,7 +14,7 @@ class AppWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: lightColorTheme,
       darkTheme: darkColorTheme,
-      home: HomeScreen(),
+      home: const MainView(),
       onInit: () => Get.put<UserModel>(UserModel(), tag: "user"),
     );
   }
